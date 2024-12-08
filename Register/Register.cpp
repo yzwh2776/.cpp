b8eddf901463fea61dc8c8010dc37e3e5045e708
Dylan <mitchelldylan964@gmail.com>
2024-12-08 10:00:14 -0500

    Created Register project

---
+++ Register/Register.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstring>
+#include <bits/stdc++.h>
+
+class Register {
+
+	std::map<std::string, float> Menu 
+
+/*
+// Burgers
+	;Menu["Big Mac"]= 5.29
+	;Menu["Quarter Pounder with Cheese"]= 1
+	;Menu["Quarter Pounder with Cheese and Bacon"]= 1
+	;Menu["Deluxe Quarter Pounder with Cheese"]= 1
+	;Menu["Double Quarter Pounder with Cheese"]= 1
+	;Menu["Cheeseburgers"]= 1
+	;Menu["Double Quarter Pounder with Cheese and Bacon"]= 1
+	;Menu["Deluxe Double Quarter Pounder with Cheese"]= 1
+
+// Chicken Sandwhiches
+	;Menu["McCrispy"]= 1
+	;Menu["Spicy McCrispy"]= 1
+	;Menu["Deluxe McCrispy"]= 1
+	;Menu["Spicy Deluxe McCrispy"]= 1
+	;Menu["Filet-O-Fish"]= 1
+	
+// Chicken Nuggets
+	;Menu["20-Piece Chicken Nuggets"]= 1
+	;Menu["6-Piece Chicken Nuggets"]= 1
+	;Menu["10-Piece Chicken Nuggets"]= 1
+
+// Beverages
+	;float small= 1.59
+	;Menu["Small Coke"]= small 
+	;Menu["Small Diet Coke"]= small
+	;Menu["Small Coke Zero"]= small
+	;Menu["Small Dr.Pepper"]= small
+	;Menu["Small Sprite"]= small
+	;Menu["Small Fanta Orange"]= small
+	;Menu["Small Orange Hi-C"]= small
+	;Menu["Smal Vernors"]= small
+	
+	;float medium= 1.69
+	;Menu["Medium Coke"]= medium
+	;Menu["Medium Diet Coke"]= medium
+	;Menu["Medium Coke Zero"]= medium
+	;Menu["Medium Dr.Pepper"]= medium
+	;Menu["Medium Sprite"]= medium
+	;Menu["Medium Fanta Orange"]= medium
+	;Menu["Medium Orange Hi-C"]= medium
+	;Menu["Medium Vernors"]= medium
+
+	;float large= 1.79
+	;Menu["Large Coke"]= large
+	;Menu["Large Diet Coke"]= large
+	;Menu["Large Coke Zero"]= large
+	;Menu["Large Dr.Pepper"]= large
+	;Menu["Large Sprite"]= large
+	;Menu["Large Fanta Orange"]= large
+	;Menu["Large Orange Hi-C"]= large
+	;Menu["Large Vernors"]= large
+
+// Sides
+	;Menu["McDouble"]= 2.49
+	;Menu["McChicken"]= 2.49
+	;Menu["4-Piece Chicken Nuggets"]= 2.49
+	;Menu["Small Fries"]= 2.49
+	;Menu["Medium Fries"]= 1
+	;Menu["Large Fries"]= 1
+*/
+
+	;void greet_Customer() {
+		
+		std::cout << "Welcome to McDonalds!\n"
+		;std::cout << "What can I get for you?\n"
+		;std::string order
+		;std::cin >> order
+	
+		;std::cout << order
+	
+		;}
+
+	;}	
+
+;int main() {
+
+/*
+	const int rows= 36
+	;const int cols= 128
+	
+	;char str[rows * cols]
+	;memset(str, '\u0020', rows * cols)
+
+	;str[0]= '\u250F'
+	;std::cout << str
+*/
+
+	Register myRegister= new Register()	
+
+	;myRegister.greet_Customer()
+
+	;return 0
+	
+	;}
+
